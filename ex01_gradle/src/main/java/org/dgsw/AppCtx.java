d800e43518b6214a979d143756b8d2bff2c61746
yongbin <rhdydqls1024@naver.com>
2022-09-07 14:11:35 +0900

    :memo: :: ex01

--- ex01_gradle/src/main/java/org/dgsw/AppCtx.java
+++ ex01_gradle/src/main/java/org/dgsw/AppCtx.java
@@ -1,9 +1,11 @@
 package org.dgsw;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"org.dgsw"})
 public class AppCtx {
 
     @Bean
---
+++ ex01_springboot/src/main/java/com/dgsw/ex01_springboot/service/CService.java
@@ -0,0 +1,12 @@
+package com.dgsw.ex01_springboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CService {
+
+    public void doC() {
+        System.out.println("C doC");
+    }
+
+}
